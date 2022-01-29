32c146407ced663c141c4a4cc7e962b10cb2f69a
FrankDongxdxd <90066812+FrankDongxdxd@users.noreply.github.com>
2022-01-28 22:36:32 -0500

    Add files via upload

---
+++ dogvscat.py
@@ -0,0 +1,17 @@
+from sre_parse import CATEGORIES
+import cv2
+import tensorflow as tf
+from unidecode import Cache
+
+CATEGORIES = ["Dog","Cat"]
+
+def prepare(filepath):
+    IMG_SIZE  = 50
+    img_array=cv2.imread(filepath,cv2.IMREAD_GRAYSCALE)
+    new_array  =cv2.resize(img_array,(IMG_SIZE,IMG_SIZE))
+    return new_array.reshape(-1,IMG_SIZE,IMG_SIZE,1)
+
+model = tf.keras.models.load_model("64x2CNN.model")
+
+prediction = model.predict([prepare('cat.jpg')])
+print(CATEGORIES[int(prediction[0][0])])
